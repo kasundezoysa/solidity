b5b068b2c7e894d9e125dad8eee314d0462d5d36
Kasun De Zoysa <kasundezoysa@users.noreply.github.com>
2018-09-28 23:48:08 +0530

    Create dAppToken.sol

---
+++ dAppToken.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.2;
+
+contract dAppToken {
+    string  public name = "dApp Token";
+    string  public symbol = "DAPP";
+    string  public standard = "DApp Token v1.0";
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    function DappToken (uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        return true;
+    }
+}
